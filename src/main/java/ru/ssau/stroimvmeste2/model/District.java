f62b6d50a632481930441a82ecd078c27afbf404
nan-mot <nantodevelop@gmail.com>
2022-05-27 01:37:19 +0400

    Without db project files.

--- src/main/java/ru/ssau/stroimvmeste2/model/District.java
+++ src/main/java/ru/ssau/stroimvmeste2/model/District.java
@@ -1,12 +1,16 @@
 package ru.ssau.stroimvmeste2.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.ArrayList;
 
-
+@Entity
+@Table
 @Data
 public class District {
 
