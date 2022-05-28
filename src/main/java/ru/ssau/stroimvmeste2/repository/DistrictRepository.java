cf918245f54b27d5c9056dd4af8730030dae7313
nan-mot <nantodevelop@gmail.com>
2022-05-29 00:05:46 +0400

    files

---
+++ src/main/java/ru/ssau/stroimvmeste2/repository/DistrictRepository.java
@@ -0,0 +1,7 @@
+package ru.ssau.stroimvmeste2.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.ssau.stroimvmeste2.model.District;
+
+public interface DistrictRepository extends CrudRepository<District, Integer> {
+}
